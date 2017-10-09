a9d7397623d71f6afe0154c367ccc85886046f49
michael-mulligan <31669632+michael-mulligan@users.noreply.github.com>
2017-10-09 14:20:44 -0400

    Fixed the use of player 1 and player 2

--- guessGame/guessGame.js
+++ guessGame/guessGame.js
@@ -1,13 +1,18 @@
-var secretNumber = Math.floor(Math.random() * 9) + 1; 
+var secretNumber
 var guess = 0;
 var attempts = 0;
 
+function enterNumber(){
+	secretNumber = document.getElementById("num-input").value;
+	document.getElementById("p1-container").style.display = "none";
+	document.getElementById("p2-container").style.display = "block";
+	document.getElementById("result").innerHTML = "Player 2: Enter a guess."
+}
+
 function guessingGame(){
 	guess = document.getElementById("input").value;
-	if (guess > 10 || guess < 1){
-		document.getElementById("result").innerHTML = "Your guess is invalid!";
-	}
-	else if (guess > secretNumber){
+	
+	if (guess > secretNumber){
 		attempts++
 		document.getElementById("result").innerHTML = "Your guess is TOO HIGH! Please try again. Attempt(s) = " + attempts;
 	}
@@ -18,8 +23,7 @@ function guessingGame(){
 	else {
 		attempts++
 		document.getElementById("result").innerHTML = "The secret number was " + secretNumber + ". You guessed it in " + attempts + " attempt(s)!";
-		document.getElementById("input").style.display = "none";
-		document.getElementById("submit").style.display = "none";
+		document.getElementById("p2-container").style.display = "none";
 		document.getElementById("play-again").style.display = "block";
 	}
 }
