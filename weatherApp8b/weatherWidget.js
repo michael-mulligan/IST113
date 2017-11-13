742b5155d2d2886e72d32374c0eae8ec5b510e74
michael-mulligan <31669632+michael-mulligan@users.noreply.github.com>
2017-11-13 15:35:03 -0500

    Added weatherApp8b

---
+++ weatherApp8b/weatherWidget.js
@@ -0,0 +1,65 @@
+function WeatherWidget ($widget)
+{
+	this.update = function()
+	{
+		$(".results", $widget).hide();
+		$(".loading", $widget).show();
+		getLocation();
+		getWeatherReport();
+	};
+	
+	function getWeatherReport()
+	{
+		$.get("data/weather.json", {
+			t: new Date().getTime()
+		})
+			.done(function(data)
+			{
+				populateWeather(data);
+			})
+			.fail(function(jqXHR, textStatus, errorThrown)
+			{
+				showError(errorThrown);
+			});
+	}
+	
+	function populateWeather(data)
+	{
+		var observation = data.current_observation;
+		
+		$(".results header img", $widget).attr("src", observation.icon_url);
+			
+		$(".location>span", $widget).text(data.location.city);
+			
+		$(".conditions>span").each(function(i, e)
+		{
+			var $span = $(this);
+			var field = $span.data("field");
+			$(this).text(observation[field]);
+		});
+		
+		$(".loading", $widget).fadeOut(function ()
+		{
+			$(".results", $widget).fadeIn();
+		});
+	}
+	
+	function getLocation()
+	{
+		if (navigator.geolocation)
+		{
+			navigator.geolocation.getCurrentPosition(
+			function(position)
+			{
+				$("#latitude").val(position.coords.latitude);
+				$("#longitude").val(position.coords.longitude);
+			},
+			function(error)
+			{
+				$("#controls .error")
+					.text("ERROR: " + error.message)
+					.slideDown();
+			});
+		}
+	}
+}
