4591677a6605401560d11fa5b3e0aca4bc7340fd
michael-mulligan <31669632+michael-mulligan@users.noreply.github.com>
2017-11-20 16:36:36 -0500

    Added Jeopardy Game

---
+++ apiGame1a/game.js
@@ -0,0 +1,33 @@
+var jData;
+
+function Game(){
+	
+	$("#getQuestion").on('click', function(){
+		GetQuestion();
+	});
+	
+	$("#showAnswer").on('click', function(){
+		$("#data_answer").removeClass("hidden").html(jData[0].answer);
+	});
+};
+
+function GetQuestion(){
+	$("#data_answer").addClass("hidden");
+	$.ajax({
+			url: "http://jservice.io/api/random",
+			dataType: "json"
+		})
+		.done(function(data){
+			jData = data;
+			$("#data_category").text(data[0].category.title.toUpperCase());
+			$("#data_question").text(data[0].question);
+		})
+		.fail(function(jqXHR, textStatus, errorThrown){
+			//change this later
+			console.log(errorThrown);
+		});
+}
+
+window.onload = function() {
+	Game();
+};
