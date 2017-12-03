8a22d12aea32451168f11b508a253c880f4164c6
michael-mulligan <31669632+michael-mulligan@users.noreply.github.com>
2017-12-03 14:25:27 -0500

    Added features
    - Scoring
    - Answering logic

--- apiGame1b/game.js
+++ apiGame1b/game.js
@@ -2,10 +2,14 @@ var data0;
 var data1;
 var data2;
 
+var score = 0;
+
 function Game(){
 	
 	$("#startGame").on("click", function(){
-		StartGame();
+		$("#cat0").children("h2").text(data0[0].category.title.toUpperCase());
+		$("#cat1").children("h2").text(data1[0].category.title.toUpperCase());
+		$("#cat2").children("h2").text(data2[0].category.title.toUpperCase());
 		$("#introBox").addClass("hidden");
 	});
 	
@@ -13,9 +17,12 @@ function Game(){
 		var category = $(this).data().category;
 		var index = $(this).data().index;
 		$(this).on("click", function(){
+			$(this).addClass("nobutton").text("");
 			GetQuestion(category, index);
 		});
 	});
+	
+	$("#score").text("$" + score);
 };
 
 function StartGame(){
@@ -44,33 +51,106 @@ function StartGame(){
 			console.log(errorThrown);
 		})
 	});
-	
-	
 }
 
 function GetQuestion(category, index){
+	var answer;
+	var cashValue = -1;
+	
+	$("#answerPrompt").removeClass("hidden");
+	$("#AnswerResult").addClass("hidden");
+	$("#inputAnswer").val("")
 	
 	switch (category)
 	{
 		case 0:
 			$("#clue").text(data0[index].question);
+			answer = data0[index].answer;
+			cashValue = data0[index].value;
 			break;
 		case 1:
 			$("#clue").text(data1[index].question);
+			answer = data1[index].answer;
+			cashValue = data1[index].value;
 			break;
 		case 2:
 			$("#clue").text(data2[index].question);
+			answer = data2[index].answer;
+			cashValue = data2[index].value;
 			break;
 	}
 	
 	$("#clueBox").removeClass("hidden");
 	
 	$("#submitAnswer").on("click", function(){
+		$("#answerPrompt").addClass("hidden");
+		ProcessAnswer(answer, cashValue);
+	});
+	
+	$("#skip").on("click", function(){
 		$("#clueBox").addClass("hidden");
 	});
 }
 
+function ProcessAnswer(answer, cashValue){
+	var playerAnswer;
+	var check = -1;
+	
+	playerAnswer = $("#inputAnswer").val();
+	check = CheckAnswer(playerAnswer, answer);
+	$("#playerAnswer").text(playerAnswer);
+	$("#answer").text(answer);
+	
+	$("#AnswerResult").removeClass("hidden");
+	if (check < 4)
+	{
+		$("#result").addClass("green").removeClass("red").text("Correct!")
+	}
+	else
+	{
+		$("#result").addClass("red").removeClass("green").text("Incorrect!")
+	}
+	
+	$("#addValue").off().on("click", function(){
+		score += cashValue;
+		$("#clueBox").addClass("hidden");
+		$("#score").text("$" + score);
+	});
+	
+	$("#subtractValue").off().on("click", function(){
+		score -= cashValue;
+		$("#clueBox").addClass("hidden");
+		$("#score").text("$" + score);
+	});
+}
+
+function CheckAnswer(a, b) {
+	var t = [], u, i, j, m = a.length, n = b.length;
+	
+	if (!m) 
+	{ 
+		return n;
+	}
+	
+	if (!n)
+	{
+		return m;
+	}
+	
+	for (j = 0; j <= n; j++)
+	{
+		t[j] = j;
+	}
+	for (i = 1; i <= m; i++)
+	{
+		for (u = [i], j = 1; j <= n; j++)
+		{
+			u[j] = a[i - 1] === b[j - 1] ? t[j - 1] : Math.min(t[j - 1], t[j], u[j - 1]) + 1;
+		} t = u;
+	} return u[n];
+}
 
 window.onload = function() {
 	Game();
+	StartGame();
 };
